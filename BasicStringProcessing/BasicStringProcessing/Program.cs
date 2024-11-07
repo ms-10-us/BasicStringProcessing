f3bf4490df90bc85c0f57e62e36f3a015015bf9a
Mazen Saber <mazensaber1996@gmail.com>
2024-11-07 05:47:35 -0500

    Basic String Processing Lab - Mazen Saber

---
+++ BasicStringProcessing/BasicStringProcessing/Program.cs
@@ -0,0 +1,84 @@
+﻿Console.WriteLine("Description 1:");
+
+Console.WriteLine();
+
+string userAnswer = "y";
+
+while (userAnswer.ToLower().Trim() == "y")
+{
+    Console.WriteLine("Please enter a sentence");
+    string sentence = Console.ReadLine();
+    string word = "";
+    foreach (char character in sentence)
+    {
+        if (character != ' ')
+        {
+            word += character; 
+        }
+        else if (character == ' ' || character == '.' || character == '?' || character == '!')
+        {
+            Console.WriteLine(word);
+            word = string.Empty;
+        }
+    }
+    Console.WriteLine(word);
+
+    Console.WriteLine("Would you like to continue (y/n)?");
+    userAnswer = Console.ReadLine();
+}
+
+if (userAnswer.ToLower().Trim() == "n")
+{
+    Console.WriteLine("Goodbye!");
+}
+
+Console.WriteLine();
+
+Console.WriteLine("Description 2");
+
+Console.WriteLine();
+
+userAnswer = "y";
+List<string> userList = new List<string>();
+
+while (userAnswer.ToLower().Trim() == "y")
+{
+    Console.WriteLine("Please enter some text");
+    string userWord = Console.ReadLine();
+    userList.Add(userWord);
+    string sentence = "";
+    foreach (string wordInList in userList)
+    {
+        sentence = sentence + wordInList + " ";
+    }
+
+    Console.WriteLine($"You have entered: {sentence}");
+    sentence = string.Empty;
+    Console.WriteLine("Would you like to continue? (y/n)");
+    userAnswer = Console.ReadLine();
+}
+
+if (userAnswer.ToLower().Trim() == "n")
+{
+    Console.WriteLine("Goodbye!");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
